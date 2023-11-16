34f6d2e3658276b21be31e8dafdd764e8ccaa0fc
davrikn <david_knotten@hotmail.no>
2023-11-16 01:03:49 +0100

    reducing validation set in train

--- train.py
+++ train.py
@@ -21,7 +21,7 @@ val_losses = []
 val_accuracies = []
 
 def project_out(out, colored=False):
-    projected = np.zeros(shape=(4, 256, 512))
+    projected = np.zeros(shape=(out.shape[0], out.shape[2], out.shape[3]))
     for i in range(len(out)):
         for w in range(out.shape[2]):
             for h in range(out.shape[3]):
@@ -46,7 +46,7 @@ def train():
     images = listdir(imgroot)
     val_images = listdir(val_imgroot)
 
-    batch_size = 4
+    batch_size = 8
     epochs = 50
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -71,7 +71,7 @@ def train():
         bar = tqdm(range(math.floor(len(images)/batch_size)))
         for i in bar:
             optimizer.zero_grad()
-            bar.set_description(f"Epoch {epoch} loss: {acum_loss/(i*4+1)} accuracy: {round((correct/(total+1))*100, 1)}% val loss:{val_loss/(val_ct+1)} val accuracy: {round((val_correct / (val_total + 1)) * 100, 1)}")
+            bar.set_description(f"Epoch {epoch} loss: {acum_loss/(i*batch_size+1)} accuracy: {round((correct/(total+1))*100, 1)}% val loss:{val_loss/(val_ct+1)} val accuracy: {round((val_correct / (val_total + 1)) * 100, 1)}")
 
             image_names = images[i:i+batch_size]
             batch_img = torch.tensor(load_images(image_names), dtype=torch.float32).to(device)
@@ -83,7 +83,7 @@ def train():
             acum_loss += loss.item()
 
             out = out.cpu().detach().numpy()
-            if i % 50 == 0:
+            if (i*batch_size) % 200 == 0:
                 # Calculate loss and accuracy of train
                 projected = project_out(out)
                 batch_labels = batch_labels.cpu().detach().numpy()
@@ -94,8 +94,8 @@ def train():
 
                 # Calculate loss and accuracy of validation
                 np.random.shuffle(val_images)
-                val_imgs = torch.tensor(load_images(val_images, True), dtype=torch.float32).to(device)
-                val_labels = torch.tensor(load_labels(val_images, True), dtype=torch.long).to(device)
+                val_imgs = torch.tensor(load_images(val_images[0:batch_size], True), dtype=torch.float32).to(device)
+                val_labels = torch.tensor(load_labels(val_images[0:batch_size], True), dtype=torch.long).to(device)
 
                 out = model(val_imgs)['out']
                 val_loss += criterion(out, val_labels).item()
@@ -104,11 +104,11 @@ def train():
                 batch_labels = val_labels.cpu().detach().numpy()
                 val_correct += (projected == batch_labels).sum().item()
                 val_total += batch_labels.size
-                val_ct += 4
+                val_ct += batch_size
 
 
         accuracies.append(round((correct/total)*100, 1))
-        losses.append(acum_loss/(i*4))
+        losses.append(acum_loss/(i*batch_size))
         val_accuracies.append(round((val_correct/val_total)*100, 1))
         val_losses.append(val_loss/val_ct)
 
